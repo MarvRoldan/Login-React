e00abcfc7cb589f7abb48c2d7514d4cd1782cb74
MarvRoldan <marvinroldan_@hotmail.com>
2020-04-13 23:01:38 -0600

    Setting up login page

---
+++ src/components/LoginPage.tsx
@@ -0,0 +1,20 @@
+import React, { Component, Fragment } from 'react';
+
+export class LoginPage extends React.Component {
+    state = {
+        userName: " ",
+        password: " ",
+        userNameMessage: " ",
+        passwordMessage: " "
+    }; 
+
+    render() {
+        return (
+            <Fragment>
+                
+            </Fragment>
+        );
+    }
+}
+
+export default LoginPage;
