6ad46cda38d7f447471ca4d37bb5c0a399ca9b38
MarvRoldan <marvinroldan_@hotmail.com>
2020-04-14 00:21:31 -0600

    Cleaned up

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,48 +1,82 @@
-import React, { Component, Fragment } from "react";
-import LoginButton from "./LoginButton";
+import React from "react";
 
 const defaultState = {
-  userName: " ",
-  password: " ",
-  errorMessage: " ",
+  name: "",
+  password: "",
+  nameError: "",
+  passwordError: "",
 };
 
 export class LoginPage extends React.Component {
   state = defaultState;
 
-  valid = () => {
-    let errorMessage: string;
-
-    if (!this.state.userName.includes("user")) {
-      errorMessage = "Username is incorrect";
-    }
+  handleChange = (event: any) => {
+    const isCheckbox = event.target.type === "checkbox";
+    this.setState({
+      [event.target.name]: isCheckbox
+        ? event.target.checked
+        : event.target.value,
+    });
   };
 
-  handleLogin = (event: any) => {
-    event.preventDefault();
-    const signedIn = this.valid();
-    if (signedIn) {
-      console.log(this.state);
+  valid = () => {
+    let nameError = "";
+    let passwordError = "";
 
+    if (!this.state.name.includes("user")) {
+      nameError = "Username is incorrect";
+    }
+
+    if (!this.state.password.includes("password")) {
+      passwordError = "Password is incorrect";
+    }
+
+    if (nameError || passwordError) {
+      this.setState({ nameError, passwordError });
+      return false;
+    }
+
+    return true;
+  };
+
+  handleSubmit = (event: any) => {
+    event.preventDefault();
+    const isValid = this.valid();
+    if (isValid) {
+      console.log(this.state);
+      // clear form
       this.setState(defaultState);
     }
   };
 
   render() {
     return (
-      <Fragment>
+      <form onSubmit={this.handleSubmit}>
         <div>
-          UserName
-          <input />
-          <div>{this.state.errorMessage}</div>
+          Username
+          <input
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+          <div style={{ fontSize: 11, color: "red" }}>
+            {this.state.nameError}
+          </div>
         </div>
         <div>
           Password
-          <input type="password" />
-          <div>{this.state.errorMessage}</div>
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+          <div style={{ fontSize: 11, color: "red" }}>
+            {this.state.passwordError}
+          </div>
         </div>
-        <LoginButton />
-      </Fragment>
+        <button type="submit">submit</button>
+      </form>
     );
   }
 }
